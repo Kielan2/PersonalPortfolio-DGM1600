c2c070e12bb1fb3895cee7d761ee957d269609e0
Kielan Anderson <10972573@uvu.edu>
2021-10-07 11:12:20 -0600

    Added Star Wars files

---
+++ codejournal.js
@@ -0,0 +1,21 @@
+//Java Tips
+[Is for arrays]
+{Is for objects}
+
+
+//Java Tags
+let string1 = "hello world!"
+
+let myNum = 2039349854;
+
+let  myDecimal = 73.4 //Also called a float
+
+//Boolean
+
+let myBool = true;
+
+//Array
+
+let myArray = [] //This is an empty array
+
+let myArray = [zero, one, two,three] //Arrays start at zero
