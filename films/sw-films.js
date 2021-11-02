87b2187ed161f12732cc01d31ed2c8a27f17dfb7
Kielan Anderson <10972573@uvu.edu>
2021-11-02 17:55:51 -0600

    film page fixed

--- films/sw-films.js
+++ films/sw-films.js
@@ -1,15 +1,15 @@
 import { films } from '../data/films.js'
 
-let filmList = document.querySelector('#filmList')
+let filmList = document.querySelector('#filmlist')
 
 for (let i = 0; i < films.length; i++) {
     let figure = document.createElement('figure')
-    let figImg = document.createElement('img')
+    let myImg = document.createElement('img')
     myImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     let figCaption = document.createElement('figcaption')
-    figcaption.textContent = films[i].title
+    figCaption.textContent = films[i].title
 
-    filmList.appendChild(figImg)
+    filmList.appendChild(myImg)
     figure.appendChild(figCaption)
     filmList.appendChild(figure)
 }
