aa008a044f13063565efd8cea8687d06af0c4e61
Kielan Anderson <10972573@uvu.edu>
2021-11-01 18:58:35 -0600

    made javascipt control the dom

--- codejournal.js
+++ codejournal.js
@@ -2,13 +2,18 @@
 [Is for arrays]
 {Is for objects}
 
+var name; // a declared variable, but not initialized and it's in the global scope (BAD)
+
+let foo; // a declared variable that can be changed
+
+const ANSWER; // a declared variable that cannot be changed
 
 //Java Tags
 let string1 = "hello world!"
 
 let myNum = 2039349854;
 
-let  myDecimal = 73.4 //Also called a float
+let  myDecimal = 73.4 // Also called a float
 
 //Boolean
 
@@ -16,6 +21,6 @@ let myBool = true;
 
 //Array
 
-let myArray = [] //This is an empty array
+let myArray = [] // This is an empty array
 
-let myArray = [zero, one, two,three] //Arrays start at zero
+let myArray = [zero, one, two,three] // Arrays start at zero
