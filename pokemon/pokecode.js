91fa389729b92640f53d545df30d43621d40fc35
Kielan Anderson <10972573@uvu.edu>
2021-12-06 22:56:05 -0700

    pokepage upgrade

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,20 +1,56 @@
-async function getAPIData(url) {
+import { removeChildren } from '../utils/index.js'
+
+function getAPIData(url) {
     try {
-        const response = await fetch(url)
-        const data = await response.json()
-        return data
+        return fetch(url).then((data) => data.json())
     } catch (error) {
         console.error(error)
     }
 }
 
-getAPIData(`https://pokeapi.co/api/v2/pokemon/25`)
-.then((data) => {
-    console.log(data)
-    populatePokeCards(data)
-})
+function loadPokemon(offset = 0, limit = 25) {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`)
+.then(async (data) => {
+    for (const pokemon of data.results) {
+        await getAPIData(pokemon.url).then(pokeData => populatePokeCards(pokeData))
+    }
+})}
 
 const pokeGrid = document.querySelector('.pokeGrid')
+const loadButton = document.querySelector('.loadPokemon')
+loadButton.addEventListener('click', () => {
+    removeChildren(pokeGrid)
+    loadPokemon()
+})
+
+const newButton = document.querySelector('.newPokemon')
+newButton.addEventListener('click', () => {
+    let pokeName = prompt('What is the name of your new Pokemon?')
+    let pokeHeight = prompt('What is the height of your new Pokemon?')
+    let pokeWeight = prompt('What is the weight of your new Pokemon?')
+    let pokeAbilities = prompt('What are your Pokemon abilities? (use a comma separated list)')
+    let pokeType = prompt('What is your Pokemon type?')
+    let newPokemon =  new Pokemon(pokeName, pokeHeight, pokeWeight, getAbilitiesArray(pokeAbilities), pokeType)
+    populatePokeCards(newPokemon)
+})
+
+const morePokemon = document.querySelector('.morePokemon')
+morePokemon.addEventListener('click', () => {
+    let startPoint = prompt('Which Pokemon ID do we start with?')
+    let howMany = prompt('How many more Pokemon do you want to see?')
+    loadPokemon(startPoint, howMany)
+})
+
+function getAbilitiesArray(commaString) {
+    let tempArray = commaString.split(',')
+    return tempArray.map((abilityName) => {
+        return {
+            ability: {
+                name: abilityName
+            }
+        }
+    })
+}
 
 function populatePokeCards(singlePokemon) {
     const pokeScene = document.createElement('div')
@@ -25,12 +61,10 @@ function populatePokeCards(singlePokemon) {
         pokeCard.classList.toggle('is-flipped'))
 
     const front = populateCardFront(singlePokemon)
-    const pokeBack = document.createElement('div')
-    pokeBack.className = 'cardFace back'
-    pokeBack.textContent = 'Back'
+    const back = populateCardBack(singlePokemon)
     
     pokeCard.appendChild(front)
-    pokeCard.appendChild(pokeBack)
+    pokeCard.appendChild(back)
     pokeScene.appendChild(pokeCard)
     pokeGrid.appendChild(pokeScene)
 }
@@ -39,10 +73,41 @@ function populateCardFront(pokemon) {
     const pokeFront = document.createElement('figure')
     pokeFront.className = 'cardFace front'
     const pokeImg = document.createElement('img')
-    pokeImg.src = `https://raw.githubusercotnent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+    if(pokemon.id === 9001) {
+        pokeImg.src = '../images/OIP.jfif'
+    } else {
+    pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
     const pokeCaption = document.createElement('figcaption')
-    pokeCaption.textContent = 'Front'
+    pokeCaption.textContent = `${pokemon.id} ${pokemon.name}`
     pokeFront.appendChild(pokeImg)
     pokeFront.appendChild(pokeCaption)
     return pokeFront
 }
+
+function populateCardBack(pokemon) {
+    const pokeBack = document.createElement('div')
+    pokeBack.className = 'cardFace back'
+    const label = document.createElement('h4')
+    label.textContent = 'Abilities'
+    const abilityList = document.createElement('ul')
+    pokemon.abilities.forEach((ability) => {
+        let abilityItem = document.createElement('li')
+        abilityItem.textContent = ability.ability.name
+        abilityList.appendChild(abilityItem)
+    })
+    pokeBack.appendChild(label)
+    pokeBack.appendChild(abilityList)
+    return pokeBack 
+}
+
+class Pokemon {
+    constructor(name, height, weight, abilities, type) {
+        this.id = 9001,
+        this.name = name, 
+        this.height = height,
+        this.weight = weight,
+        this.abilities = abilities,
+        this.type = type
+    }
+}
+
